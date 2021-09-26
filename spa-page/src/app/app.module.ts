8ef41cc7fea71aa6f6fd0e759f364ab0c6f576de
Weronika Strzyżewska <w.strzyzewska@gmail.com>
2021-09-26 09:32:54 +0200

    First changes

--- spa-page/src/app/app.module.ts
+++ spa-page/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OnasComponent } from './onas/onas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
